0c2bb77ad9f6832484ed3a33249e6a8c17d06a3e
ArhabZuhan <Zuhanarsa@gmail.com>
2022-06-09 19:35:00 +0700

    dimulai dari saya membuat From

--- DisconnectedEnvironment/Form1.cs
+++ DisconnectedEnvironment/Form1.cs
@@ -3,70 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace DisconnectedEnvironment
 {
     public partial class Form1 : Form
     {
-        DataTable dt;
-        DataRow dr;
-        string code;
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
-            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
-            txtCode.Enabled = false;
-            txtName.Enabled = false;
-            txtAddress.Enabled = false;
-            txtState.Enabled = false;
-            txtCountry.Enabled = false;
-            cbDesignation.Enabled = false;
-            cbDepartment.Enabled = false;
-            cbDesignation.Items.Add("MANAGER");
-            cbDesignation.Items.Add("AUTHOR");
-            cbDesignation.Items.Add("Designer");
-            cbDepartment.Items.Add("MARKETING");
-            cbDepartment.Items.Add("FINANCE");
-            cbDepartment.Items.Add("IDD");
-            cmdSave.Enabled = false;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
-            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
-            txtCode.Enabled = false;
-            txtName.Enabled = false;
-            txtAddress.Enabled = false;
-            txtState.Enabled = false;
-            txtCountry.Enabled = false;
-            cbDesignation.Enabled = false;
-            cbDepartment.Enabled = false;
-            cbDesignation.Items.Add("MANAGER");
-            cbDesignation.Items.Add("AUTHOR");
-            cbDesignation.Items.Add("Designer");
-            cbDepartment.Items.Add("MARKETING");
-            cbDepartment.Items.Add("FINANCE");
-            cbDepartment.Items.Add("IDD");
-            cmdSave.Enabled = false;
         }
     }
 }
